abf295c4c22b14644d5c646ce7d6ef2156a43fe0
develpercriscr <crislydevelopercr@gmail.com>
2026-05-23 19:22:01 -0600

    Agrega Biblioteca y Main con menu funcional

---
+++ LAB_U2_Herencia_GitHub/src/Biblioteca.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+
+
+public class Biblioteca {
+    private ArrayList<Material> materiales;
+
+    public Biblioteca() {
+        materiales = new ArrayList<>();
+    }
+
+    public void agregarMaterial(Material material) {
+        materiales.add(material);
+        System.out.println("Material agregado correctamente.");
+    }
+
+    public void listarMateriales() {
+        for (Material m : materiales) {
+            m.mostrarInfo();
+        }
+    }
+
+    public Material buscarPorId(int id) {
+        for (Material m : materiales) {
+            if (m.getId() == id) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public void prestarMaterial(int id) {
+        Material m = buscarPorId(id);
+
+        if (m != null) {
+            m.prestar();
+        } else {
+            System.out.println("Material no encontrado.");
+        }
+    }
+
+    public void devolverMaterial(int id) {
+        Material m = buscarPorId(id);
+
+        if (m != null) {
+            m.devolver();
+        } else {
+            System.out.println("Material no encontrado.");
+        }
+    }
+}
